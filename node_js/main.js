768953c95c7bf5cfd1236da24a35848db69110a5
tunagiant <68101999+tunagiant@users.noreply.github.com>
2021-01-18 15:48:25 +0900

    add

---
+++ node_js/main.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname + _url);
+    response.end(fs.readFileSync(__dirname + _url));
+ 
+});
+app.listen(3000);
